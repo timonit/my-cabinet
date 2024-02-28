bc9bf698d1fecd7744e754b5cb08626591d4d1c1
timonit <timonsus@gmail.com>
2024-02-28 15:59:15 +0500

    feature[city-picker]: add highlight selected item

--- src/features/city/ui/city-picker.vue
+++ src/features/city/ui/city-picker.vue
@@ -10,7 +10,12 @@ const cityStore = useCityStore();
 
     <v-menu activator="parent">
       <v-list @update:selected="(items: any[]) => cityStore.select(items[0])">
-        <v-list-item v-for="item in cityStore.cities" :key="item.code" :value="item.code">
+        <v-list-item
+          v-for="item in cityStore.cities"
+          :key="item.code"
+          :value="item.code"
+          :active="cityStore.selected && item.code === cityStore.selected.code"
+        >
           <v-list-item-title>{{ item.name }}</v-list-item-title>
         </v-list-item>
       </v-list>
