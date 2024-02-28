75ea2740af10e8e1c998ee0ab00863bded5f1b77
timonit <timonsus@gmail.com>
2024-02-28 21:41:21 +0500

    fix[company-card]: split company-card to status list

---
+++ src/entities/company/ui/status-list.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { useCompanyStore } from '@/entities/company/model';
+import type { Level } from './types';
+import StatusListItem from './status-list-item.vue';
+
+const companyStore = useCompanyStore();
+
+const computeLevel = (val: number): Level => {
+  if (val < 100) return 'low';
+  if (val > 130) return 'high';
+  if (val >= 100) return 'medium';
+  return 'unknown';
+};
+</script>
+
+<template>
+  <v-list lines="two">
+    
+    <StatusListItem
+      label="Количество товаров:"
+      :value="companyStore.getStatusProp('totalProducts')"
+      :level="computeLevel(companyStore.getStatusProp('totalProducts'))"
+    />
+    
+    <StatusListItem
+      label="Товар в наличии:"
+      :value="companyStore.getStatusProp('availableProducts')"
+      :level="computeLevel(companyStore.getStatusProp('availableProducts'))"
+    />
+    
+    <StatusListItem
+      label="Товар без наличии:"
+      :value="companyStore.getStatusProp('nonAvailableProducts')"
+      :level="computeLevel(companyStore.getStatusProp('nonAvailableProducts'))"
+    />
+
+  </v-list>
+</template>
